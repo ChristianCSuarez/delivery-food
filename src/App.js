56bb066b95c1a957ff73206b9538c6848bf4dd0b
Christian Cantillo <chris8610@gmail.com>
2020-07-04 21:57:23 -0500

    Add google fonts and install styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>SliceLine</h1>
+      <GlobalStyle />
+      <div>Hello Jefe Cesar</div>
+    </>
   );
 }
 
